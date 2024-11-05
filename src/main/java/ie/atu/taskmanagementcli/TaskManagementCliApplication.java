471ec099b824fd12d674ca2ba5523dc529cbab75
James A <G00379271@atu.ie>
2024-11-05 11:40:05 +0000

    Initial commit, project skeleton

---
+++ src/main/java/ie/atu/taskmanagementcli/TaskManagementCliApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.taskmanagementcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementCliApplication.class, args);
+	}
+
+}
